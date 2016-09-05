5fe00c7ec08c15190bb2cb13ff2aed06ede0447e
lionlies <lionlies@gmail.com>
2016-09-05 11:15:51 +0800

    Removed welcome controller and it's route.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :ideas
   resources :favorite_ideas, only: [:create, :destroy]
 
-  get "welcome/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
