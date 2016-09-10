5143a46604520ff02eb85228f03ce940c267a1f5
lionlies <lionlies@gmail.com>
2016-09-10 19:50:27 +0800

    Added Idea model validation, changed title of website.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :email, :username, uniqueness: true
   has_many :ideas, dependent: :destroy
-  
+
   has_many :favorites
   has_many :favorite_ideas, through: :favorites, source: :favorited, source_type: "Idea"
 end
